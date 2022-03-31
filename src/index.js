512784736aca0f6d935829b19c1602c23c6eacb8
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-03-31 10:09:27 -0400

    upgrade openweather geolocation api endpoint from http to https

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ function getForecast(city) {
 
 async function getCoordinates(city) {
     const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}` +
+        `https://api.openweathermap.org/geo/1.0/direct?q=${city}` +
         '&limit=1&appid=72e691cc8e68804d3b51462e3f5c963f',
         { mode: 'cors' }
     );
